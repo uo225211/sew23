4d2134868e40b51dd9f0ed760de8928e7e36b307
uo225211 <71693558+uo225211@users.noreply.github.com>
2023-11-10 15:32:52 +0100

    Add files via upload

---
+++ js/pais.js
@@ -0,0 +1,56 @@
+class Pais {
+    constructor (nombrePais,nombreCapital,poblacion,formaGobierno,coordenadasCapital,religionMayoritaria){
+        this.nombrePais=nombrePais;
+        this.nombreCapital=nombreCapital;
+        this.poblacion=poblacion;
+        this.formaGobierno=formaGobierno;
+        this.coordenadasCapital=coordenadasCapital;
+        this.religionMayoritaria=religionMayoritaria;
+    }
+
+    constructor (nombrePais,nombreCapital,poblacion){
+        this.nombrePais=nombrePais;
+        this.nombreCapital=nombreCapital;
+        this.poblacion=poblacion;        
+    }
+
+    rellenarDatos (formaGobierno,coordenadasCapital,religionMayoritaria){
+        this.formaGobierno=formaGobierno;
+        this.coordenadasCapital=coordenadasCapital;
+        this.religionMayoritaria=religionMayoritaria;
+    }
+
+    getNombrePais(){
+        return ""+this.nombrePais;
+    }
+    getNombreCapital(){
+        return ""+this.nombreCapital;
+    }
+    getPoblacion(){
+        return ""+this.poblacion;
+    }
+    getFormaGobierno(){
+        return ""+this.formaGobierno;
+    }
+    getCoordenadasCapital(){
+        return ""+this.coordenadasCapital;
+    }
+    getReligionMayoritaria(){
+        return ""+this.religionMayoritaria;
+    }
+
+    infoSecundaria(){
+        return "<ul> <li> "+this.poblacion+"</li> <li>"+this.formaGobierno+"</li> <li>"+this.religionMayoritaria+"</li> </ul>";
+    }
+    infoCoordenadas(){
+        document.write("<p>Las coordenadas de"+this.getNombreCapital+" son: "+this.coordenadasCapital+". </p>");
+    }
+}
+ var republica=new Pais("Republica Checa", "Praga", "10510000", "República parlamentaria", "50.07487550135318, 14.431291625910324" ,"Cristianismo" );
+ 
+ 
+ document.write("<p>Nombre: "+ republica.getNombrePais+"</p>");
+ document.write("<p>Capital: "+ republica.getNombreCapital+"</p>");
+ republica.infoCoordenadas();
+
+ 
